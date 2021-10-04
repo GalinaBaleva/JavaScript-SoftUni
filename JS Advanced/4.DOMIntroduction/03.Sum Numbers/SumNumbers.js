1777da856cfa59894783004d56b7a06fe8e5cd46
Galina <baleva1990@gmail.com>
2021-10-04 23:24:26 +0200

    DOM Introduction

---
+++ JS Advanced/4.DOMIntroduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,13 @@
+function calc() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    document.getElementById("sum").value = Number(num1) + Number(num2);
+    // const num1 = document.getElementById('num1').value;
+    // const num2 = document.getElementById('num2').value;
+    
+    // let sum = Number(num1) + Number(num2);
+    
+    // document.getElementById('sum').value = sum;
+}
+
---
+++ JS Advanced/4.DOMIntroduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  const text = document.getElementById('text');
+  text.style.display = 'inline';
+
+  const hideText = document.getElementById('more');
+  hideText.style.display = 'none';
+}
