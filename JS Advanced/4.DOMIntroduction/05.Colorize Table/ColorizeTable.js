66f21b5195f45da8f49d0995a63feb277b7c1b2a
Galina <baleva1990@gmail.com>
2021-10-05 17:02:41 +0200

    new

--- JS Advanced/4.DOMIntroduction/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/4.DOMIntroduction/05.Colorize Table/ColorizeTable.js
@@ -1,5 +1,6 @@
 function colorize() {
-    const elements = Array.from(document.querySelectorAll("table tr"));
+    const elements = Array.from(document.querySelectorAll("table tr")); //('table tr:nth-child(even)')селектира нечетните индекси
+    
     
     for (let i = 1; i < elements.length; i += 2) {
         elements[i].style.background = 'teal';
