1c2c6e155d63208b57826485234e0e6b7f34452f
Galina <baleva1990@gmail.com>
2021-10-15 00:17:31 +0200

    + one more version

--- JS Advanced/DOM Manipulation and Events/02. Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,14 +1,21 @@
-function deleteByEmail() {
-    const writedText = document.querySelector('input[name="email"]').value;
-    let rows = Array.from(document.querySelector('tbody').children);
+// function deleteByEmail() {
+//     const writedText = document.querySelector('input[name="email"]').value;
+//     let rows = Array.from(document.querySelector('tbody').children);
 
-    let flag = false;
-    
-    for(let row of rows){
-        if(writedText === row.children[1].textContent){
-            row.remove();
-            flag = true;
-        }
-    }
-    document.getElementById('result').textContent = flag === true ? `Deleted.` : `Not found.`;
+//     let flag = false;
+
+//     for(let row of rows){
+//         if(writedText === row.children[1].textContent){
+//             row.remove();
+//             flag = true;
+//         }
+//     }
+//     document.getElementById('result').textContent = flag === true ? `Deleted.` : `Not found.`;
+// }
+function deleteByEmail() {
+    const text = document.querySelector('input[name="email"]').value;
+    let rows = Array.from(document.querySelector("tbody").children).filter(r => r.children[1].textContent === text);
+    rows.forEach(row => row.remove());
+
+    document.getElementById('result').textContent = rows.letngth > 0 ? `Deleted.` : `Not found.`
 }
