4a098061d906e11cada1e21d30ddc764bd0d8cef
Galina <baleva1990@gmail.com>
2021-10-19 23:45:56 +0200

    new exercise

---
+++ JS Advanced/5.DOM Manipulation and Events/Exercises/01. Sections/script.js
@@ -0,0 +1,20 @@
+function create(words) {
+   const contentDiv = document.getElementById('content');
+   let eventListener = contentDiv.addEventListener('click', onClick);
+
+   for (let word of words) {
+      const newDivElement = document.createElement('div');
+      const newParagraf = document.createElement('p');
+      newParagraf.textContent = word;
+      newParagraf.style.display = 'none'
+
+      newDivElement.appendChild(newParagraf);
+      contentDiv.appendChild(newDivElement)
+   }
+
+   function onClick(ev) {
+      
+      const sectionStyle = ev.target.firstChild;
+      sectionStyle.style.display = '';
+   }
+}
