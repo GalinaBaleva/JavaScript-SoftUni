c9996a51594c3395e583c059cf6aacb81357aead
Galina <baleva1990@gmail.com>
2021-11-09 23:39:32 +0100

    new

--- JS Advanced/06.AdvancedFunctions/Lab/2.Add.js
+++
@@ -1,8 +0,0 @@
-function solution(a) {
-               return (b) => {
-                              return a + b
-               }
-}
-let add5 = solution(5);
-console.log(add5(2));
-console.log(add5(3));
