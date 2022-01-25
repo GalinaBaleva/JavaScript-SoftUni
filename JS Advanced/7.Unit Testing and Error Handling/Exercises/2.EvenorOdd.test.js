4bce6c45bdbb42e8713bf5e4c460fdbd6fd276bc
Galina <baleva1990@gmail.com>
2022-01-25 22:19:05 +0100

    new

--- JS Advanced/7.Unit Testing and Error Handling/Exercises/2.EvenorOdd.test.js
+++ JS Advanced/7.Unit Testing and Error Handling/Exercises/2.EvenorOdd.test.js
@@ -1,20 +1,21 @@
 const { expect } = require('chai');
-const evenOrOdd = require('./2.EvenorOdd');
+const isOddOrEven = require('./2.EvenorOdd');
 
 describe('oddOrEvenString', () => {
                it('not a string', () => {
-                              expect(evenOrOdd(1)).to.be.undefined;
-                              expect(evenOrOdd([134])).to.be.undefined;
-                              expect(evenOrOdd({})).to.be.undefined;
+                              expect(isOddOrEven(1)).to.be.undefined;
+                              expect(isOddOrEven(134)).to.be.undefined;
                });
                it('string with odd length', () => {
-                              expect(evenOrOdd('GfT')).to.be.equal('odd');
-                              expect(evenOrOdd('gFg')).to.be.equal('odd');
+                              expect(isOddOrEven('GfT')).to.be.equal('odd');
+                              expect(isOddOrEven('gFg')).to.be.equal('odd');
+                              expect(isOddOrEven('FFF')).to.be.equal('odd');
+                              expect(isOddOrEven('aaa')).to.be.equal('odd');
                });
                it('string with even length', () => {
-                              expect(evenOrOdd('fJfk')).to.be.equal('even');
-                              expect(evenOrOdd('FJfY')).to.be.equal('even');
-                              expect(evenOrOdd('')).to.be.equal('even');
-                              expect(evenOrOdd()).to.be.equal('even');
+                              expect(isOddOrEven('fJfk')).to.be.equal('even');
+                              expect(isOddOrEven('FJfY')).to.be.equal('even');
+                              expect(isOddOrEven('FFFF')).to.be.equal('even');
+                              expect(isOddOrEven('aaaa')).to.be.equal('even');
                })
 })
