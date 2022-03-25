32f633cde3a6ea2ac6a627fe716cf6feaa07c64d
Galina <baleva1990@gmail.com>
2022-03-25 13:02:46 +0100

    next part

--- JS Advanced/ExamPreparation/1.Pet_Me - Resources/solution.js
+++ JS Advanced/ExamPreparation/1.Pet_Me - Resources/solution.js
@@ -13,6 +13,8 @@ function solve() {
         if (container.map(a => a.value.trim()).some(a => a === '') || isNaN(Number(age))) {
             return;
         } else {
+            let buttonContact = el('button', `Contact with owner`)
+            buttonContact.addEventListener('click', toYesTakeIt);
             let pet = el('li',
                 el('p',
                     el('strong', name),
@@ -22,24 +24,42 @@ function solve() {
                     el('strong', kind),
                 ),
                 el('span', `Owner: ${currentOwner}`),
-                el('button', `Contact with owner`)
+                buttonContact
             )
 
             adoptionCont.appendChild(pet)
+            container.map(el => el.value = '');
         }
     }
-}
-function el(type, ...text) {
-    let element = document.createElement(type);
 
-    for (let word of text) {
-        if (typeof word === 'string' || typeof word === 'number') {
-            word = document.createTextNode(word);
+    function toYesTakeIt(e){
+        let parent = e.target.parentNode;
+        let buttonToRemove = parent.querySelector('button');
+        buttonToRemove.remove()
+
+        let divTag = el('div', 
+
+        )
+    }
+    function elWithAtr(type, atr, ...text){
+        let element = document.createElement(type);
+
+        if()
+    }
+
+    function el(type, ...text) {
+        let element = document.createElement(type);
+
+        for (let word of text) {
+            if (typeof word === 'string' || typeof word === 'number') {
+                word = document.createTextNode(word);
+            }
             element.appendChild(word);
         }
+        return element;
     }
 
-
+    
 }
 
 
