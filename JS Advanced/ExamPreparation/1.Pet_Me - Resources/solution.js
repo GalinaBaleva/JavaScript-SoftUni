0609b53e561e8ef8be1c59a19a95bf1fb2b1e403
Galina <baleva1990@gmail.com>
2022-03-26 23:17:40 +0100

    ready

--- JS Advanced/ExamPreparation/1.Pet_Me - Resources/solution.js
+++ JS Advanced/ExamPreparation/1.Pet_Me - Resources/solution.js
@@ -10,7 +10,7 @@ function solve() {
     function onAdoptionContainer(e) {
         e.preventDefault()
         let [name, age, kind, currentOwner] = container.map(a => a.value.trim());
-        if (container.map(a => a.value.trim()).some(a => a === '') || isNaN(Number(age))) {
+        if (container.map(a => a.value.trim()).some(a => a == '') || Number.isNaN(Number(age))) {
             return;
         } else {
             let buttonContact = el('button', `Contact with owner`)
@@ -18,9 +18,9 @@ function solve() {
             let pet = el('li',
                 el('p',
                     el('strong', name),
-                    'is a',
+                    ' is a ',
                     el('strong', age),
-                    'year old',
+                    ' year old ',
                     el('strong', kind),
                 ),
                 el('span', `Owner: ${currentOwner}`),
@@ -32,19 +32,57 @@ function solve() {
         }
     }
 
-    function toYesTakeIt(e){
+    function toYesTakeIt(e) {
         let parent = e.target.parentNode;
         let buttonToRemove = parent.querySelector('button');
         buttonToRemove.remove()
+        let takeItButton = el('button', 'Yes! I take it!');
 
-        let divTag = el('div', 
-
+        let divTag = el('div',
+            elWithAtr('input', { placeholder: 'Enter your names' }),
+            takeItButton
         )
+        takeItButton.addEventListener('click', toAdoptedContainer)
+        parent.appendChild(divTag);
     }
-    function elWithAtr(type, atr, ...text){
+
+    function toAdoptedContainer(e) {
+        let parent = e.target.parentNode.parentNode;
+        let newOwner = parent.querySelector('div input')
+        if(!newOwner.value){
+            return;
+        }
+        newOwner = newOwner.value;
+        let info = Array.from(parent.querySelectorAll('p strong'));
+        let [name, age, kind] = info.map(a => a.textContent.trim())
+        let buttonChecked = el('button', `Checked`)
+        buttonChecked.addEventListener('click', checkedFunc);
+        let pet = el('li',
+            el('p',
+                el('strong', name),
+                ' is a ',
+                el('strong', age),
+                ' year old ',
+                el('strong', kind),
+            ),
+            el('span', `New Owner: ${newOwner}`),
+            buttonChecked
+        );
+        adopted.appendChild(pet);
+        parent.remove()
+    }
+
+    function checkedFunc(e) {
+        e.target.parentNode.remove();
+    }
+
+    function elWithAtr(type, atr, ...text) {
         let element = document.createElement(type);
 
-        if()
+        for (let atribut in atr) {
+            element[atribut] = atr[atribut];
+        };
+        return element;
     }
 
     function el(type, ...text) {
@@ -58,8 +96,6 @@ function solve() {
         }
         return element;
     }
-
-    
 }
 
 
