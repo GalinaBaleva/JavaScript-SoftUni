5f33e6414cbda99c5d9509be39d4ee764acaa36b
Galina <baleva1990@gmail.com>
2022-04-10 15:28:36 +0200

    100/100

--- JS Advanced/9.Prototypes and Inheritance/Exercises/5.People.js
+++ JS Advanced/9.Prototypes and Inheritance/Exercises/5.People.js
@@ -1,63 +1,63 @@
 function people() {
-               class Employee {
-                              constructor(name, age) {
-                                if(new.target == Employee){
-                                                            throw new Error(`Error`);
-                                             }
-                                             this.name = name;
-                                             this.age = age;
-                                             this.salary = 0;
-                                             this.tasks = [];
-                                             this.counter = 0;
-                              }
-                              work() {
-                                             console.log(this.tasks[this.counter]);
-                                             this.counter += 1;
-                                             if (this.tasks.length - 1 < this.counter) {
-                                                            this.counter = 0;
-                                             }
-                              };
-                              collectSalary() {
-                                             console.log(`${this.name} received ${this.salary} this month.`);
-                              };
-               };
-               class Junior extends Employee {
-                              constructor(name, age) {
-                                             super(name, age);
-                                             this.tasks = [`${this.name} is working on a simple task.`]
+  class Employee {
+    constructor(name, age) {
+      if (new.target == Employee) {
+        throw new Error(`Error`);
+      }
+      this.name = name;
+      this.age = age;
+      this.salary = 0;
+      this.tasks = [];
+      this.counter = 0;
+    }
+    work() {
+      console.log(this.tasks[this.counter]);
+      this.counter += 1;
+      if (this.tasks.length - 1 < this.counter) {
+        this.counter = 0;
+      }
+    };
+    collectSalary() {
+      console.log(`${this.name} received ${this.salary} this month.`);
+    };
+  };
+  class Junior extends Employee {
+    constructor(name, age) {
+      super(name, age);
+      this.tasks = [`${this.name} is working on a simple task.`]
 
-                              };
-               };
-               class Senior extends Employee {
-                              constructor(name, age) {
-                                             super(name, age);
-                                             this.tasks = [
-                                                            `${this.name} is working on a complicated task.`,
-                                                            `${this.name} is taking time off work.`,
-                                                            `${this.name} is supervising junior workers.`
-                                             ]
-                              }
-               };
-               class Manager extends Employee {
-                              constructor(name, age) {
-                                             super(name, age);
-                                             this.dividend = 0;
-                                             this.tasks = [
-                                                            `${this.name} scheduled a meeting.`,
-                                                            `${this.name} is preparing a quartely report.`
-                                             ];
+    };
+  };
+  class Senior extends Employee {
+    constructor(name, age) {
+      super(name, age);
+      this.tasks = [
+        `${this.name} is working on a complicated task.`,
+        `${this.name} is taking time off work.`,
+        `${this.name} is supervising junior workers.`
+      ]
+    }
+  };
+  class Manager extends Employee {
+    constructor(name, age) {
+      super(name, age);
+      this.dividend = 0;
+      this.tasks = [
+        `${this.name} scheduled a meeting.`,
+        `${this.name} is preparing a quarterly report.`
+      ];
 
-                              };
-                              collectSalary() {
-                                             console.log(`${this.name} received ${this.salary + this.dividend} this month.`)
-                              }
-               };
-               return {
-                              Employee,
-                              Junior,
-                              Senior,
-                              Manager
-               };
+    };
+    collectSalary() {
+      console.log(`${this.name} received ${this.salary + this.dividend} this month.`)
+    }
+  };
+  return {
+    Employee,
+    Junior,
+    Senior,
+    Manager
+  };
 };
 
 
