9f8e939021f23b3850367ed847c7fe48f3e2a3d3
Galina <baleva1990@gmail.com>
2022-04-21 22:31:34 +0200

    .

--- JS Aplication/01.XHR/scripts/app.js
+++ JS Aplication/01.XHR/scripts/app.js
@@ -1,15 +1,4 @@
 function loadRepos() {
-   // let url = "https://api.github.com/users/testnakov/repos";
-   // const httpsRequest = new XMLHttpRequest();
-   // httpsRequest.addEventListener('readystatechenge', function (){
-   //    if(httpsRequest.readyState == 4 && httpsRequest.status == 200){
-   //       document.getElementById('res').textContent = httpsRequest.responseText;
-   //    };
-   //});
-
-   
-   // httpsRequest.open('GET', url);
-   // httpsRequest.send();
    const repoUrl="https://api.github.com/users/testnakov/repos";
    let req = new XMLHttpRequest();
    req.onreadystatechange = function()
