cf2e4ba3e04eb47006867d1b0393284e21eaa26e
Galina <baleva1990@gmail.com>
2022-04-26 11:48:52 +0200

    async/await/+try/catch

--- JS Application/1.Asynchronous Programming/01.Bus-Stop/app.js
+++ JS Application/1.Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,29 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const input = document.getElementById('stopId').value;
+    const resultName = document.getElementById('stopName');
+    const resutlBuses = document.getElementById('buses');
+
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${input}`;
+
+
+    try {
+        resultName.textContent = `Loading...`;
+        resutlBuses.replaceChildren()
+        const response = await fetch(url);
+        if (response.status != 200) {
+            throw new Error(`Error`);
+        }
+        const data = await response.json();
+        console.log(data)
+
+        resultName.textContent = data.name;
+        Object.entries(data.buses).forEach(el => {
+            let liElement = document.createElement('li');
+            liElement.textContent = `Bus ${el[0]} arrives in ${el[1]} minutes`
+            resutlBuses.appendChild(liElement);
+        })
+
+    } catch (error) {
+        resultName.textContent = `Error`;
+    }
 }
