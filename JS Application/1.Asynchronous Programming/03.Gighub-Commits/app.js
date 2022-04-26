007c94bf24d16bc24961d0d2a5e0dbb8cdccf2ae
Galina <baleva1990@gmail.com>
2022-04-26 22:52:02 +0200

    adding new exercise

--- JS Application/1.Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS Application/1.Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,27 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+
+    const userName = document.getElementById('username').value;
+    const userRepo = document.getElementById('repo').value;
+    const url = `https://api.github.com/repos/${userName}/${userRepo}/commits`;
+
+    let ulList = document.getElementById('commits');
+
+    try {
+        ulList.replaceChildren();
+        let response = await fetch(url);
+        if(response.status != 200){
+            throw new Error(response.status);
+        }
+        let data = await response.json();
+
+        data.forEach(element => {
+            let liElement = document.createElement('li');
+            liElement.textContent = `${element.commit.author.name}: ${element.commit.message}`;
+            ulList.appendChild(liElement);
+        });
+    } catch(error){
+        let liElement = document.createElement('li');
+        liElement.textContent = `Error: ${error.message} (Not Found)`;
+        ulList.appendChild(liElement);
+    }
 }
