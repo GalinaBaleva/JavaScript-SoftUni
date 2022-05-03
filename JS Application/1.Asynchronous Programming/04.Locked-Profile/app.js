109a154dde40993f9444fb16af2bae019adda6a3
Galina <baleva1990@gmail.com>
2022-05-03 22:17:00 +0200

    first part

--- JS Application/1.Asynchronous Programming/04.Locked-Profile/app.js
+++ JS Application/1.Asynchronous Programming/04.Locked-Profile/app.js
@@ -1,3 +1,10 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    let url = `http://localhost:3030/jsonstore/advanced/profiles`;
+
+    let result = await fetch(url);
+    if(result.status != 200){
+        throw new Error(`Error`);
+    }
+    let result2 = await result.json();
+    Object.entries(result2).forEach(el => console.log(el))
 }
