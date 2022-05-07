ce60283527a7ad39a0079bdef31101e19c659d5d
Galina <baleva1990@gmail.com>
2022-05-07 23:52:55 +0200

    new exercise, first part

--- JS Application/1.Asynchronous Programming/05. Accordion/accordion.js
+++ JS Application/1.Asynchronous Programming/05. Accordion/accordion.js
@@ -1,3 +1,32 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const urlList = `http://localhost:3030/jsonstore/advanced/articles/list`;
+
+    const respons = await fetch(urlList);
+    const data = await respons.json();
+
+    console.log(data)
+
+};
+
+function el(tagName, className, atr, ...text){
+    const tag = document.creatElement(tagName);
+
+    if(className != undefined){
+        tag.classList = className;
+    };
+
+    if(atr != undefined){
+        tag.setAttribute(atr[0], atr[1]);
+    };
+
+    if(text.length > 0){
+        for(let word of text){
+            word = document.createTextNode(word);
+        };
+        tag.appendChild(word);
+    };
+    return tag;
+
 }
+solution();
+
