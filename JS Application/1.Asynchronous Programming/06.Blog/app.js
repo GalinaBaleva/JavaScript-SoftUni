2a3de9fce353329728e953f2af2e84063a775910
Galina <baleva1990@gmail.com>
2022-05-11 12:54:18 +0200

    first part

--- JS Application/1.Asynchronous Programming/06.Blog/app.js
+++ JS Application/1.Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,35 @@
 function attachEvents() {
-    console.log('TODO...');
+    const btnLoad = document.getElementById('btnLoadPosts');
+    btnLoad.addEventListener('click', loadingPosts);
+};
+async function loadingPosts() {
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const result = await data;
+    console.log(result)
+    
+    const selectSection = document.getElementById('posts');
+    Object.values(result).forEach(element => {
+        console.log(element.id)
+    });
 }
 
+function el(tagName, atr, ...text) {
+    const tag = document.createElement(tagName);
+
+    if (atr[0] != undefined) {
+        tag.setAttribute(atr[0], atr[1]);
+    }
+    if (text[0] != undefined) {
+        for (let word of text) {
+            if (word == 'string' || word == 'number') {
+                word = document.createTextNode(word);
+            };
+            tag.appendChild(word);
+        };
+    };
+    return tag;
+}
 attachEvents();
