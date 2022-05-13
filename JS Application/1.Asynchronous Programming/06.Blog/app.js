20373e08aba0222938f0d690bc1f3244352223d8
Galina <baleva1990@gmail.com>
2022-05-13 11:28:20 +0200

    added the last part

--- JS Application/1.Asynchronous Programming/06.Blog/app.js
+++ JS Application/1.Asynchronous Programming/06.Blog/app.js
@@ -3,19 +3,16 @@ function attachEvents() {
     btnLoad.addEventListener('click', loadingPosts);
     const btnView = document.getElementById('btnViewPost');
 
-
     const selectSection = document.getElementById('posts');
     const postTitle = document.getElementById('post-title');
     const postBody = document.getElementById('post-body');
     const commentSection = document.getElementById('post-comments');
-
 
     async function loadingPosts() {
         const url = `http://localhost:3030/jsonstore/blog/posts`;
         if (selectSection.children.length != 0) {
             selectSection.replaceChildren()
         }
-
         const response = await fetch(url);
         const data = await response.json();
         const result = await data;
@@ -31,43 +28,30 @@ function attachEvents() {
     }
 
     async function viewList(res) {
+        postTitle.textContent = 'Loading...';
+        postBody.textContent = 'Loading...';
         const comments = `http://localhost:3030/jsonstore/blog/comments/`;
         const value = selectSection.value;
         const findedPost = Object.entries(res).find(el => el[0] == selectSection.value);
 
+        if(commentSection.children.length != 0){
+            commentSection.replaceChildren();
+        }
         if (value != '') {
             const response = await fetch(comments);
             const data = await response.json();
             const result = Object.entries(data).filter(el => el[1].postId == value);
-            result.forEach(elm => console.log(elm));
             result.forEach(elm => {
-                const tag = el('li', ['id', elm[1].postId], elm[1].text);
+                const tag = document.createElement('li');
+                tag.setAttribute('id', elm[1].postId)
+                tag.textContent = elm[1].text;
                 commentSection.appendChild(tag);
             })
            
             postTitle.textContent = findedPost[1].title;
             postBody.textContent = findedPost[1].body;
         }
-
     };
-
-    function el(tagName, atr, ...text) {
-        const tag = document.createElement(tagName);
-
-        if (atr[0] != undefined) {
-            tag.setAttribute(atr[0], atr[1]);
-        }
-        if (text[0] != undefined) {
-            for (let word of text) {
-                if (word == 'string' || word == 'number') {
-                    word = document.createTextNode(word);
-                };
-                tag.appendChild(word)
-            };
-        };
-
-        return tag;
-    }
 };
 
 attachEvents();
