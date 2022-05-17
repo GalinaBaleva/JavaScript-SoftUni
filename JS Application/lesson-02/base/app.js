4ba9606bf412c2cb8591e581599f9d5d1a8d77a3
Galina <baleva1990@gmail.com>
2022-05-17 22:10:46 +0200

    new

--- JS Application/lesson-02/base/app.js
+++ JS Application/lesson-02/base/app.js
@@ -1,30 +1,58 @@
-async function getRecipes(){
+async function getRecipes() {
                const response = await fetch(`http://localhost:3030/jsonstore/cookbook/recipes`);
                const data = await response.json();
+
                return data;
-             
+
 
 };
-window.addEventListener('load', async ()=> {
+window.addEventListener('load', async () => {
+               const mainTag = document.getElementsByTagName('main')[0];
                const data = await getRecipes()
-               Array.from(data).forEach(element => console.log(element))
-})
+               Object.values(data).forEach(element => {
+                              const imgTag = document.createElement('img');
+                              imgTag.setAttribute('src', element.img)
+                              const articleTag = el('article', 'preview',
+                                             el('div', 'title', el('h2', undefined, element.name)),
+                                             el('div', 'small', imgTag));
+                              
+                              mainTag.firstChild.remove();
+                              mainTag.appendChild(articleTag);
+                              articleTag.addEventListener('click', async () => {
+                                             let promise = getByID(element._id);
+                                             addingIngredients(articleTag, promise, mainTag);         
+                              });
+               });
+});
+async function addingIngredients(articleTag, promise, mainTag){
+               console.log(articleTag, promise, mainTag)
 
-// function el(type, className, ...content){
-//                const tag = document.createElement(type);
+}
+async function getByID(id) {
+               const url = `http://localhost:3030/jsonstore/cookbook/details/`;
+               const response = await fetch(url + id);
+               const data = response.json();
 
-//                if(className != undefined){
-//                               tag.classList = className;
-//                };
+               return data;
+               
+};
 
-//                if(content[0] != undefined){
-//                               for(let word of content){
-//                                              if(typeof word == 'string' || typeof word == 'number'){
-//                                                             word = document.createTextNode(word);
-//                                              };
-//                                              tag.appendChild(word);
-//                               };
-//                };
-//                return tag;
-// }
+
+function el(type, className, ...content) {
+               const tag = document.createElement(type);
+
+               if (className != undefined) {
+                              tag.classList = className;
+               };
+
+               if (content[0] != undefined) {
+                              for (let word of content) {
+                                             if (typeof word == 'string' || typeof word == 'number') {
+                                                            word = document.createTextNode(word);
+                                             };
+                                             tag.appendChild(word);
+                              };
+               };
+               return tag;
+}
 
