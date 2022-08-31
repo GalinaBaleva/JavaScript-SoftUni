42ccbc28d7164a5c083a8065c1d64012d52b2a9e
Galina <baleva1990@gmail.com>
2022-08-31 22:57:17 +0200

    new functionality

--- JS Application/1.Asynchronous Programming/01.Bus-Stop/app.js
+++ JS Application/1.Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,28 +1,77 @@
 async function getInfo() {
-    const input = document.getElementById('stopId').value;
-    const resultName = document.getElementById('stopName');
-    const resutlBuses = document.getElementById('buses');
+    const button = document.getElementById('submit');
+    button.disabled = true;
+    const stopId = document.getElementById('stopId');
+    const stopName = document.getElementById('stopName');
+    const timeTable = document.getElementById('buses');
+    const url = `http://localhost:3030/jsonstore/bus/businfo/` + stopId.value;
 
-    let url = `http://localhost:3030/jsonstore/bus/businfo/${input}`;
-
+    stopName.textContent = `Loading...`;
+    timeTable.replaceChildren();
 
     try {
-        resultName.textContent = `Loading...`;
-        resutlBuses.replaceChildren()
         const response = await fetch(url);
+
         if (response.status != 200) {
             throw new Error(`Error`);
         }
         const data = await response.json();
 
-        resultName.textContent = data.name;
-        Object.entries(data.buses).forEach(el => {
-            let liElement = document.createElement('li');
-            liElement.textContent = `Bus ${el[0]} arrives in ${el[1]} minutes`
-            resutlBuses.appendChild(liElement);
-        })
+        stopName.textContent = data.name;
+
+        Object.entries(data.buses).forEach(bus => {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+            timeTable.appendChild(liElement);
+        });
+
+        button.disabled = false;
 
     } catch (error) {
-        resultName.textContent = `Error`;
+        timeTable.replaceChildren();
+        stopName.textContent = `Error`;
+        button.disabled = false;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getInfo() {
+//     const input = document.getElementById('stopId').value;
+//     const resultName = document.getElementById('stopName');
+//     const resutlBuses = document.getElementById('buses');
+
+//     let url = `http://localhost:3030/jsonstore/bus/businfo/${input}`;
+
+
+//     try {
+//         resultName.textContent = `Loading...`;
+//         resutlBuses.replaceChildren()
+//         const response = await fetch(url);
+//         if (response.status != 200) {
+//             throw new Error(`Error`);
+//         }
+//         const data = await response.json();
+
+//         resultName.textContent = data.name;
+//         Object.entries(data.buses).forEach(el => {
+//             let liElement = document.createElement('li');
+//             liElement.textContent = `Bus ${el[0]} arrives in ${el[1]} minutes`
+//             resutlBuses.appendChild(liElement);
+//         })
+
+//     } catch (error) {
+//         resultName.textContent = `Error`;
+//     }
+// }
