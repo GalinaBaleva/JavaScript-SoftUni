ccbbcaf4ac62b27fcb6a44c714a50e8715da2206
Galina <baleva1990@gmail.com>
2022-10-18 12:54:27 +0200

    added the login functionality

---
+++ JS Application/2.Remote Data and Authentication/05.Fisher-Game/src/login.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+               const form = document.querySelector('form');
+               form.addEventListener('submit', onLogin);
+});
+
+async function onLogin(event) {
+               event.preventDefault();
+
+               const formData = new FormData(event.target)
+
+               const email = formData.get('email');
+               const password = formData.get('password');
+
+               try {
+                              const res = await fetch('http://localhost:3030/users/login', {
+                                             method: 'post',
+                                             headers: {
+                                                            'Content-Type': 'applications/json'
+                                             },
+                                             body: JSON.stringify({ email, password })
+                              });
+
+                              if (res.ok != true) {
+                                             const error = await res.json();
+                                             throw new Error(error.message);
+                              };
+
+                              const data = await res.json();
+                              const userData = {
+                                             email: data.email,
+                                             id: data._id,
+                                             taken: data.accessTocen
+                              };
+
+                              sessionStorage.setItem('userData', JSON.stringify(userData));
+                              window.location = './index.html';
+
+               } catch (error) {
+                              alert(error.message);
+               };
+
+
+}
+
+
