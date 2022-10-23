0f9e1ad8611c4a95b60b91ada879d314c491d228
Galina <baleva1990@gmail.com>
2022-10-23 23:59:29 +0200

    next part is added

--- JS Application/2.Remote Data and Authentication/05.Fisher-Game/src/app.js
+++ JS Application/2.Remote Data and Authentication/05.Fisher-Game/src/app.js
@@ -1,6 +1,8 @@
 let userData = null;
 
 window.addEventListener('DOMContentLoaded', () => {
+               const catches = document.querySelector('#catches');
+               catches.replaceChildren();
                userData = JSON.parse(sessionStorage.getItem('userData'));
                if (userData != null) {
                               document.getElementById('guest').style.display = 'none';
@@ -14,7 +16,6 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 async function loadData(event) {
-               const catches = document.querySelector('#catches');
                catches.replaceChildren();
 
                const res = await fetch('http://localhost:3030/data/catches');
@@ -64,10 +65,12 @@ async function onSubmit(event) {
 
 function createAngler(el) {
                const isUser = userData && userData.id == el._ownerId ? [] : ['disabled', true];
-               const updateBtn = e('button', 'update', [['data_id', el._ownerId], isUser], 'Update');
-               updateBtn.addEventListener('click', onUpdate)
-               const deleteBtn = e('button', 'delete', [['data_id', el._ownerId], isUser], 'Delete');
-               deleteBtn.addEventListener('click', onDelete)
+               const updateBtn = e('button', 'update', [['data-id', el._id], isUser], 'Update');
+               updateBtn.addEventListener('click', onUpdate);
+
+               const deleteBtn = e('button', 'delete', [['data-id', el._id], isUser], 'Delete');
+               deleteBtn.addEventListener('click', onDelete);
+
                const angler = e('div', 'catch', [],
                               e('label', '', [], 'Angler'),
                               e('input', 'angler', [['type', 'text'], ['value', el.angler], isUser], ''),
@@ -89,12 +92,43 @@ function createAngler(el) {
 };
 
 async function onUpdate(event) {
-               console.log(event);
+               const id = event.target.dataset.id;
+               const formData = event.target.parentElement.children;
+               
+
+               const body = formData.map((label, input) => Object.assign({[label.textContent]:input.value}), {})
+               console.log(body)
+
+
+
+
+               // const res = await fetch(`http://localhost:3030/data/catches/${id}`, {
+               //                method: 'put',
+               //                headers: {
+               //                               'Content-Type': 'applications/json',
+               //                               'X-Authorization': userData.token
+               //                },
+               //                body: {}
+               // });
+               // const data = res.json();
+               // console.log(data)
+
+               
 };
 
 async function onDelete(event) {
-               console.log(event);
-}
+               const parent = event.target.parentElement;
+               const id = event.target.dataset.id;
+
+               await fetch(`http://localhost:3030/data/catches/${id}`, {
+                              method: 'delete',
+                              headers: {
+                                             'Content-Type': 'applications/json',
+                                             'X-Authorization': userData.token
+                              }
+               });
+               loadData();
+};
 
 function e(tagName, className, attrNames, ...text) {
                let tag = document.createElement(tagName);
@@ -120,6 +154,8 @@ function e(tagName, className, attrNames, ...text) {
                return tag;
 };
 //welcome, chenge the name
+//logout function
+//delete and update functionality
 
 
 
