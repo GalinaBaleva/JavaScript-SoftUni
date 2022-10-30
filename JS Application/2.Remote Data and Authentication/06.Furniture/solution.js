570c830f2a59a9d705a3e40d1055e726a432426a
Galina <baleva1990@gmail.com>
2022-10-30 23:47:19 +0100

    added the new part

--- JS Application/2.Remote Data and Authentication/06.Furniture/solution.js
+++ JS Application/2.Remote Data and Authentication/06.Furniture/solution.js
@@ -1,5 +1,10 @@
 function solve() {
+
   const [first, loginForm] = document.querySelectorAll('.col-md-12 form');
+  const tbody = document.getElementsByTagName('tbody')[0];
+
+  const buyBtn = document.querySelectorAll('.col-md-12 button')[1];
+  buyBtn.addEventListener('click', toBuy);
 
   if (loginForm != undefined) {
     const registerForm = first;
@@ -14,8 +19,6 @@ function solve() {
     const createForme = first;
     createForme.addEventListener('submit', onCreate);
   };
-
-
 
   //Register
 
@@ -96,8 +99,8 @@ function solve() {
     if (body != undefined) {
       options.body = JSON.stringify(body);
     };
-    if(authToken != undefined){
-      options.headers[X-Authorization] = authToken;
+    if (authToken != undefined) {
+      options.headers['X-Authorization'] = authToken;
     }
 
     const res = await fetch(url, options);
@@ -112,17 +115,84 @@ function solve() {
     event.preventDefault();
     const dataCreate = new FormData(event.target);
     const some = [...dataCreate.values()].some(x => x == '');
-    const body = dataCreate.reduce((acc, curent) => Object.assign(acc, {curent}, {}) );
-    console.log(body)
+    const body = [...dataCreate.entries()].reduce((acc, curent) => Object.assign(acc, { [curent[0]]: curent[1] }), {});
+
     if (some === true) {
       alert(`All fields are required!`);
       return;
     };
-    //sendingRequests('http://localhost:3030/data/furniture', 'post', dataCreate, authToken)
+    const respons = await sendingRequests('http://localhost:3030/data/furniture', 'post', body, sessionStorage.authToken)
 
+    try {
+      if (respons._id === undefined) {
+        throw new Error(respons.message)
+      };
 
+      const articleToBuy = el('tr', '', [['data-id', respons._ownerId]],
+        el('td', '', [],
+          el('img', '', [['src', respons.img]])
+        ),
+        el('td', '', [],
+          el('p', '', [], respons.name)
+        ),
+        el('td', '', [],
+          el('p', '', [], respons.price)
+        ),
+        el('td', '', [],
+          el('p', '', [], respons.factor)
+        ),
+        el('td', '', [],
+          el('input', '', [['type', 'checkbox']], '')
+        ),
+      )
 
-  }
+      tbody.appendChild(articleToBuy);
 
-}
+    } catch (error) {
+      alert(error);
+    };
+  };
+
+  //function to create elements
+
+  function el(tagName, className, attrNames, ...text) {
+    let tag = document.createElement(tagName);
+
+    if (className != '') {
+      tag.className = className;
+    };
+
+    if (attrNames.length !== 0) {
+      for (let attr of attrNames) {
+        tag.setAttribute(attr[0], attr[1]);
+      };
+    };
+
+    for (let word of text) {
+      if (typeof (word) === 'string' || typeof (word) === 'number') {
+        word = document.createTextNode(word);
+      };
+      tag.appendChild(word);
+    };
+
+    return tag;
+  };
+
+  //bye product
+
+  async function toBuy(event) {
+
+    const cheked = [...tbody.querySelectorAll('input')].filter(x => x.checked);
+    const products = cheked.map(elem => elem.parentElement.parentElement);
+    const bodys = products.map(tr => {
+      const [name, price, factor] = tr.querySelectorAll('td p');
+
+      return { name: name.textContent, price: price.textContent };
+    });
+    
+    // const results = bodys.map( body => async function () { await sendingRequests('http://localhost:3030/data/orders', 'post', body, sessionStorage.authToken)});
+    const result = await sendingRequests('http://localhost:3030/data/orders', 'post', bodys[0], sessionStorage.authToken);
+    console.log(result)// Error "Invalid access token" with correct token
+  };
+};
 solve()
