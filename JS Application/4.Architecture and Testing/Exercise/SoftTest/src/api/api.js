25cdb1731281353bbbfe8ef7d3623c82d73b1ec7
GalinaBaleva <baleva1990@gmail.com>
2023-01-03 10:34:44 +0100

    new

---
+++ JS Application/4.Architecture and Testing/Exercise/SoftTest/src/api/api.js
@@ -0,0 +1,42 @@
+const host = 'http://lockalhost:3030';
+
+export async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    };
+
+    const token = sessionStorage.getItem('accessToken');
+
+    if (token) {
+        options.headers['X-Authorization'] = 'token';
+    };
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.ok != true) {
+            throw new Error(erroe.message);
+        };
+
+        if (response.startus == 204) {
+            return response;
+        };
+
+        return response.json();
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    };
+};
+
+export const get = request.bind(null, 'get');
+export const post = request.bind(null, 'post');
+export const put = request.bind(null, 'put');
+export const del = request.bind(null, 'delete');
--- JS Application/4.Architecture and Testing/Exercise/SoftTest/src/views/register.js
+++ JS Application/4.Architecture and Testing/Exercise/SoftTest/src/views/register.js
@@ -1,6 +1,33 @@
+import { post } from "../api/api.js";
+import { createSubmitHandler } from "../util.js";
+
+createSubmitHandler('registerView', onRegister);
+
 const section = document.getElementById('registerView');
 section.remove();
 
-export function registerView(ctx){
+let ctx = null
+
+export function registerView(viewFuncs){
+    ctx = viewFuncs;
     ctx.render(section);
+}
+
+function onRegister({email, password, repeatPassword}){
+    let isTrue = true;
+
+    if(email.length < 4){
+        alert('The email should be at least 3 characters long!');
+        isTrue = false;
+    };
+
+    if(password.length < 4){
+        alert('The password should be at least 3 characters long!');
+        isTrue = false;
+    };
+
+    if(password !== repeatPassword){
+        alert('Password don\'t match!');
+        isTrue = false;
+    };
 }
