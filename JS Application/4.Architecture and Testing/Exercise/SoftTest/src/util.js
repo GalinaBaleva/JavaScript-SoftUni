bc05aa4536cef001a9fc383fb7b113c558bc4b17
GalinaBaleva <baleva1990@gmail.com>
2023-01-11 09:30:02 +0100

    showing all ideas in dashboard section

--- JS Application/4.Architecture and Testing/Exercise/SoftTest/src/util.js
+++ JS Application/4.Architecture and Testing/Exercise/SoftTest/src/util.js
@@ -1,25 +1,48 @@
-export  function setUserData(data){
+export function setUserData(data) {
     sessionStorage.setItem('userId', data._id);
     sessionStorage.setItem('email', data.email);
     sessionStorage.setItem('accessToken', data.accessToken);
 };
 
-export function clearUserData(){
+export function clearUserData() {
     sessionStorage.removeItem('userId');
     sessionStorage.removeItem('email');
     sessionStorage.removeItem('accessToken');
 };
 
-export function createSubmitHandler(formId, callback){
+export function createSubmitHandler(formId, callback) {
     document.getElementById(formId).addEventListener('submit', onSubmit);
 
-    async function onSubmit(event){
+    async function onSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
         const data = Object.fromEntries(formData);
 
         callback(data, event);
     };
+};
 
+export function el(tagName, className, attrName, ...text) {
+    let tag = document.createElement(tagName);
 
+    if (className.length > 0) {
+        for(let clas of className){
+            tag.classList.add(clas);
+        }
+    };
+
+    if (attrName.length > 0) {
+        for (let attr of attrName) {
+            tag.setAttribute(attr[0], attr[1]);
+        };
+    };
+
+    for (let word of text) {
+        if (typeof (word) == 'string' || typeof (word) == 'number') {
+            word = document.createTextNode(word);
+        };
+        tag.appendChild(word);
+    };
+
+    return tag;
 }
--- JS Application/4.Architecture and Testing/Exercise/SoftTest/src/views/dashboard.js
+++ JS Application/4.Architecture and Testing/Exercise/SoftTest/src/views/dashboard.js
@@ -1,6 +1,37 @@
+import { get } from "../api/api.js";
+import { el } from "../util.js";
+
 const section = document.getElementById('dashboard-holder');
 section.remove();
 
-export function dashboardView(ctx){
+let ctx = null;
+
+
+export async function dashboardView(inCtx) {
+    ctx = inCtx;
+    section.replaceChildren();
+
+    const data = await get('/data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc');
+    let children = null;
+
+    if (data.lenght <= 0) {
+        children = el('h1', [], [], 'No ideas yet! Be the first one :)')
+    } else {
+        data.map(item => cardCreater(item));
+    };
+
+
     ctx.render(section);
+};
+
+function cardCreater(cardItem) {
+    const divCard = el('div', ['card', 'overflow-hidden', 'current-card', 'details'], [['id', cardItem._id]],
+        el('div', ['card-body'], [],
+            el('p', ['card-text'], [], cardItem.title)
+        ),
+        el('img', ['card-image'], [['src', cardItem.img], ['alt', 'Card image cap']], ''),
+        el('a', ['btn'], [], 'Details')
+    );
+    section.appendChild(divCard);
 }
+
