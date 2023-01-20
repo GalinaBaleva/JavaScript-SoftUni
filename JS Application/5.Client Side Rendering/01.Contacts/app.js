b37e98f9b20dd344773a28c5744006359d9c1c4c
GalinaBaleva <baleva1990@gmail.com>
2023-01-20 18:08:16 +0100

    new

---
+++ JS Application/5.Client Side Rendering/01.Contacts/app.js
@@ -0,0 +1,33 @@
+import { contacts } from './contacts.js';
+import { html, render, nothing } from './node_modules/lit-html/lit-html.js'
+
+const root = document.getElementById('contacts');
+
+const contactsCard = (contact) => html`
+<div class="contact card">
+    <div>
+        <i class="far fa-user-circle gravatar"></i>
+    </div>
+    <div class="info">
+        <h2>Name: ${contact.name}</h2>
+        <button class="detailsBtn" @click=${onClick.bind(null, contact.id)}>Details</button>
+        <div class="details" id="${contact.id}">
+            <p>Phone number: ${contact.phoneNumber}</p>
+            <p>Email: ${contact.email}</p>
+        </div>
+    </div>
+</div>`;
+
+render(contacts.map(contactsCard), root)
+
+
+function onClick(id, event) {
+    const index = contacts.find(x => x.id == id);
+    const div = event.target.parentElement.querySelector('div .details');
+    console.log(div.style.display = 'inline-block')
+    if (div.style.display == 'none') {
+        div.style.display == 'inline-block';
+    } else if (div.style.display == 'inline-block') {
+        div.style.display == 'none';
+    }
+}
