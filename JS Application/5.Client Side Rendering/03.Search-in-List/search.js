aed1d5e56e5f7dd5a2c05dec251d0a4e86ef2547
GalinaBaleva <baleva1990@gmail.com>
2023-01-25 16:10:24 +0100

    new

--- JS Application/5.Client Side Rendering/03.Search-in-List/search.js
+++ JS Application/5.Client Side Rendering/03.Search-in-List/search.js
@@ -4,20 +4,33 @@ import { towns } from './towns.js';
 const townsRoot = document.getElementById('towns');
 const resultRoot = document.getElementById('result');
 
+
 const surchedTown = document.getElementById('searchText');
 const button = document.querySelector('button');
-button.addEventListener('click', search);
+button.addEventListener('click', onClick);
 
 
-function loadTowns(match){
+search();
+
+function loadTowns(surchedTown) {
    return html`
    <ul>
       ${towns.map(town => html`
-         <li class="${(match && town.includes(match)) ? "active" : ""}">${town}</li>
+      <li class="${(surchedTown && town.includes(surchedTown)) ? " active" : ""}">${town}</li>
       `)}
    </ul>`;
 };
 
-function search() {
+function search(surchedTown) {
    render(loadTowns(surchedTown), townsRoot)
+};
+
+function onClick() {
+   search(surchedTown.value);
+   render(result(), resultRoot);
+};
+
+function result() {
+   const matches = document.querySelectorAll('.active');
+   return html`<p>${matches ? `${matches.length} matches found` : ""}</p>`;
 };
