c4cfebcfa9df881ab6c52a6d00bf81e17ec15004
GalinaBaleva <baleva1990@gmail.com>
2023-01-29 11:24:16 +0100

    new part

--- JS Application/5.Client Side Rendering/05.Table-Search/solution.js
+++ JS Application/5.Client Side Rendering/05.Table-Search/solution.js
@@ -6,11 +6,28 @@ const url = 'http://localhost:3030/jsonstore/advanced/table';
 const root = document.querySelector('tbody');
 document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-async function loadingStudents(data, searchedText) {
+goto();
 
-
-
+async function goto(){
+   const data = await request();
+   const users = loadingStudents(data)
+   render(users, root);
 }
+
+
+function loadingStudents(data, searchedText) {
+   const usersTempls = html`${repeat(Object.values(data), user => user._id, user => html`
+     <tr class=${searchedText && (user.firstName.include(searchedText) ||
+                     user.lastName.include(searchedText) ||
+                     user.email.include(searchedText) ||
+                     user.course.include(searchedText)) ? "yellow" : ''}>
+                     <td>${user.lastName}</td>
+                     <td>${user.email}</td>
+                     <td>${user.course}</td>
+                  </tr>
+   `)}`
+ return usersTempls;
+};
 
 
 
@@ -18,14 +35,21 @@ function onClick(event) {
    const searchField = event.target.parentElement.querySelector('#searchField').value;
    console.log(serchField)
 
-}
+};
 
 
 async function request() {
-   const resp = await fetch(url, 'GET');
+ try{  
+   const resp = await fetch(url, {method: 'GET'});
+   if(resp.ok != true){
+      throw new Error(resp.json());
+   }
    const data = await resp.json();
 
    return data;
+} catch(err){
+      alert(err.message);
+   }
 };
 
 
