4296a9ab58ab47068f14b41b99f81a01687481e5
GalinaBaleva <baleva1990@gmail.com>
2023-01-31 09:35:13 +0100

    the last part is added

--- JS Application/5.Client Side Rendering/05.Table-Search/solution.js
+++ JS Application/5.Client Side Rendering/05.Table-Search/solution.js
@@ -8,20 +8,20 @@ document.querySelector('#searchBtn').addEventListener('click', onClick);
 
 goto();
 
-async function goto(){
+async function goto(searchField){
    const data = await request();
-   const users = loadingStudents(data)
+   const users = loadingStudents(data, searchField)
    render(users, root);
 }
 
 
 function loadingStudents(data, searchedText) {
    const usersTempls = html`${repeat(Object.values(data), user => user._id, user => html`
-     <tr class=${searchedText && (user.firstName.include(searchedText) ||
-                     user.lastName.include(searchedText) ||
-                     user.email.include(searchedText) ||
-                     user.course.include(searchedText)) ? "yellow" : ''}>
-                     <td>${user.lastName}</td>
+     <tr class=${searchedText && (user.firstName.toLowerCase().includes(searchedText) ||
+                     user.lastName.toLowerCase().includes(searchedText) ||
+                     user.email.toLowerCase().includes(searchedText) ||
+                     user.course.toLowerCase().includes(searchedText)) ? 'select' : ''}>
+                     <td>${user.firstName} ${user.lastName}</td>
                      <td>${user.email}</td>
                      <td>${user.course}</td>
                   </tr>
@@ -32,8 +32,9 @@ function loadingStudents(data, searchedText) {
 
 
 function onClick(event) {
-   const searchField = event.target.parentElement.querySelector('#searchField').value;
-   console.log(serchField)
+   let searchField = event.target.parentElement.querySelector('#searchField').value;
+   goto(searchField.toLowerCase());
+   searchField = '';
 
 };
 
