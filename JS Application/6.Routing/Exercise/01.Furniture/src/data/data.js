3a7760277622e14f7fffdd5d615c97408c566341
GalinaBaleva <baleva1990@gmail.com>
2023-02-16 18:34:01 +0100

    new

---
+++ JS Application/6.Routing/Exercise/01.Furniture/src/data/data.js
@@ -0,0 +1,31 @@
+import { post } from "./api.js";
+
+const endPoints = {
+    'register': '/users/register',
+    'login': '/users/login',
+    'logout': '/users/logout',
+    'catalog': 'data/catalog',
+    'details': '/data/catalog/'
+
+};
+
+
+//Register User (POST): http://localhost:3030/users/register
+export async function postRegister(data){
+    const result = await post(endPoints.register, data);
+    return result;
+};
+
+
+
+/*
+Login User (POST): http://localhost:3030/users/login
+Logout User (GET): http://localhost:3030/users/logout
+
+Create Furniture (POST): http://localhost:3030/data/catalog
+All Furniture (GET): http://localhost:3030/data/catalog
+Furniture Details (GET): http://localhost:3030/data/catalog/:id  !!!don't forget ID
+Update Furniture (PUT): http://localhost:3030/data/catalog/:id   !!!don't forget ID
+Delete Furniture (DELETE):  http://localhost:3030/data/catalog/:id !!!don't forget ID
+My Furniture (GET): http://localhost:3030/data/catalog?where=_ownerId%3D%22{userId}%22
+*/
--- JS Application/6.Routing/Exercise/01.Furniture/src/views/register.js
+++ JS Application/6.Routing/Exercise/01.Furniture/src/views/register.js
@@ -1,7 +1,60 @@
+import { postRegister } from "../data/data.js";
 import { html } from "../lib.js";
 
 
 export async function showRegister(ctx) {
-    ctx.render(html`<p>Register</p>`);
+    ctx.render(registerTemplate);
+
+};
+
+const registerTemplate = html`  
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Register New User</h1>
+        <p>Please fill all fields.</p>
+    </div>
+</div>
+<form @submit=${onSubmit}>
+    <div class="row space-top">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="email">Email</label>
+                <input class="form-control" id="email" type="text" name="email">
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="password">Password</label>
+                <input class="form-control" id="password" type="password" name="password">
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="rePass">Repeat</label>
+                <input class="form-control" id="rePass" type="password" name="rePass">
+            </div>
+            <input type="submit"  class="btn btn-primary" value="Register" />
+        </div>
+    </div>
+</form>
+</div>`;
+
+async function onSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const emptiField = [...formData.values()].some(x => x == '');
+
+    if(emptiField){
+        alert('All fields are reqired!');
+        return;
+    };
+
+    const data = [...formData.entries()].reduce((acc, curent) => Object.assign(acc, {[curent[0]]: curent[1].trim()}), {});
+
+
+    if(data.password !== data.rePass){
+        alert('Password doesn\'t match!');
+        return;
+    };
+    
+    const response = await postRegister(data);
+
+    console.log(response)
 
 };
