8fd78c065dc79ca8db45e8efa0768d270d4265aa
GalinaBaleva <baleva1990@gmail.com>
2023-03-16 10:07:26 +0100

    new

--- JS Application/7.Modular Application/page-demo/src/views/details.js
+++ JS Application/7.Modular Application/page-demo/src/views/details.js
@@ -1,52 +1,84 @@
-import { html, nothing, until } from "../lib.js";
-import { getById } from "../data/recipes.js";
-import { getLikesByRecipeId } from "./liks.js";
+import { html, until, nothing } from '../lib.js';
+import { getById } from '../data/recipes.js';
+import '../data/likes.js';
+import { getLikesByRecipeId, likeRecipe } from '../data/likes.js';
+import { createSubmitHandler } from '../util.js';
+import { createComment, getCommentsByRecipeId } from '../data/comments.js';
+
 
 const asyncTemplate = (recipePromise) => html`
 ${until(recipePromise, recipeSkeleton())}`;
 
-const detailsTemplate = (recipe, likes, canLike) => html`
+const detailsTemplate = (recipe, likes, canLike, onLike, onComment, comments) => html`
 <h2>${recipe.name}</h2>
 <div>
-${canLike ? html`<a href='javascript:void(0)'>Like</a>` : nothing}
-${likes} like${likes == 1 ? '' : 's'}
+    ${canLike ? html`<a href="javascript:void(0)" @click=${onLike}>Like</a>` : nothing}
+    ${likes} like${likes == 1 ? '' : 's'}
 </div>
 <img src=${'/' + recipe.img}>
 <h3>Ingredients</h3>
 <ul>
     ${recipe.ingredients.map(i => html`<li>${i}</li>`)}
 </ul>
-<h3>Preparation Steps</h3>
+<h3>Preparation Step</h3>
 <ul>
     ${recipe.steps.map(s => html`<li>${s}</li>`)}
-</ul>`;
+</ul>
+<h3>Comments:</h3>
+<ul>
+    ${comments.map(c => html`<li>${c.author.username}: ${c.content}</li>`)}
+</ul>
+<div>
+    <form @submit=${onComment}>
+        <div>
+            <textarea width="500" rows="10" name="content"></textarea>
+        </div>
+        <button>Post Comment</button>
+    </form>
+</div>
+`;
 
 const recipeSkeleton = () => html`
-<h2>Recipe Detail</h2>
+<h2>Recipe Details</h2>
 <h3>Ingredients</h3>
 <ul>
-    Loding &hellip;
+    Loading &hellip;
 </ul>
-<h3>Prepatarion Step</h3>
+<h3>Preparation Step</h3>
 <ul>
-    Loding &hellip;
-</ul>`;
+    Loading &hellip;
+</ul>
+`;
 
 export function showDetails(ctx) {
     const id = ctx.params.id;
     const user = ctx.user;
     let userId;
-
-    if(user){
+    if (user) {
         userId = user._id;
     }
 
-    ctx.render(until(loadRecipe(id, userId), asyncTemplate()))
-};
+    ctx.render(asyncTemplate(loadRecipe(id, userId, onLike, createSubmitHandler(onComment))));
 
-async function loadRecipe(id, userId) {
-    const { likes , canLike } = await getLikesByRecipeId(id, userId);
+    async function onLike() {
+        await likeRecipe(id);
+        ctx.page.redirect('/recipes/' + id);
+    }
 
-    const recipe = await getById(id);
-    return detailsTemplate(recipe, likes, canLike);
-};
+    async function onComment({ content }) {
+        await createComment(id, content);
+        ctx.page.redirect('/recipes/' + id);
+    }
+}
+
+async function loadRecipe(id, userId, onLike, onComment) {
+    const [{ likes, canLike }, comments, recipe] = await Promise.all([
+        getLikesByRecipeId(id, userId),
+        getCommentsByRecipeId(id),
+        getById(id)
+    ]);
+
+    const isOwner = recipe._ownerId == userId;
+
+    return detailsTemplate(recipe, likes, canLike && !isOwner, onLike, onComment, comments);
+}
